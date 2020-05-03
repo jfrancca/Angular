5a2be5c26e2a84c6e7420da6b478925e52f70d0f
Inversiones Berman <camrod87@gmail.com>
2020-05-02 20:50:44 -0500

    Se añadieron las rutas a los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilmsComponent } from './components/films/films.component';
+import { PeopleComponent } from './components/people/people.component';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { SpeciesComponent } from './components/species/species.component';
+import { VehiclesComponent } from './components/vehicles/vehicles.component';
+import { StarshipsComponent } from "./components/starships/starships.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Peliculas', component: FilmsComponent},
+  {path:'Personajes', component: PeopleComponent},
+  {path:'Planetas', component: PlanetsComponent},
+  {path:'Especies', component: SpeciesComponent},
+  {path:'Naves', component: StarshipsComponent},
+  {path:'Vehiculos', component: VehiclesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
